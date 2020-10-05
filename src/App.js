c1d41e8d9667ffebbf8ef98c86998269a011b0d5
Shujaat Azim <shujaat.azim@gmail.com>
2020-10-04 20:47:20 -0400

    Added ability to toggle opacity of a card image. Will put in a separate component eventually.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import React, { useState, useEffect } from 'react';
 function App() {
 
   const [card, setCard] = useState("")
+  const [opacity, setOpacity] = useState("50%")
 
   useEffect(() => {
-    fetch('https://api.pokemontcg.io/v1/cards/xy7-54')
+    fetch('https://api.pokemontcg.io/v1/cards/base1-2')
     .then(resp => resp.json())
     .then(card => setCard(card))
   }, [])
 
+  const handleOpacity = () => {
+    if (opacity === "50%") {
+      return setOpacity("100%")
+    } 
+    return setOpacity("50%")
+  }
+
   return (
     <div>
-      Game!
+      Collection:
       <div>
-        {card !== "" ? <img style={{ opacity: "50%" }} alt="Pokemon Card" src={`${card.card.imageUrl}`} /> : null }
+        {card !== "" ? <img onClick={() => handleOpacity()} style={{ opacity: opacity }} alt="Pokemon Card" src={`${card.card.imageUrl}`} /> : null }
       </div>
     </div>
   );
